60b22bb1cf2d838944c588874c5e7101255ad807
kkolyszko <katarzyna.kolyszko@gmail.com>
2017-10-08 01:29:48 +0200

    fixed code adding to html

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,11 @@ var usersHeight = prompt("How tall is vour tree?");
 
 function drawTree (treeHeight) {
         star = '';
-        for (j=0; j<=treeHeight; j++){
-            document.getElementById("tree").innerHTML = document.getElementById("tree").innerHTML + star + "<br>";
+        for (i=0; i<=treeHeight; i++){
+            document.getElementById("tree").innerHTML += star + "<br>";
             star += '*';
     }
 }
 
 drawTree(usersHeight);
+
